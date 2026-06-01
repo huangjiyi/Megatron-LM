230abcf5998d885e3fff3668987e8b6b3ad2f6da
huangjiyi <947613776@qq.com>
2026-06-01 17:20:26 +0800

    uniform code

--- align.sh
+++ align.sh
@@ -1,7 +1,7 @@
 export TORCH_COMPILE_DISABLE=1
 export TORCHDYNAMO_DISABLE=1
 
-DSV4_MEGATRON_MOE_FP32_ACCUM=1 \
+FLAGS_use_accuracy_compatible_kernel=1 \
 MOE_PERMUTE_FUSION=0 \
 RUN_ID=cleanalign_minpatch_ep8_8layer_fixed_loss_1step \
 TRAIN_ITERS=1 \
